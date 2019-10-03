076dcf1488e6f531e560b9dbe99a8af204916d2f
Chris Hodgson <cmhodgson@gmail.com>
2019-10-03 13:36:41 -0700

    Added/updated license headers

--- src/main/java/ca/bc/gov/ols/config/CassandraConfigurationStore.java
+++ src/main/java/ca/bc/gov/ols/config/CassandraConfigurationStore.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright © 2008-2019, Province of British Columbia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ca.bc.gov.ols.config;
 
 import java.io.BufferedInputStream;
@@ -11,7 +26,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
@@ -25,7 +39,6 @@ import com.datastax.oss.driver.api.core.cql.SimpleStatement;
 import com.datastax.oss.driver.api.core.metadata.schema.KeyspaceMetadata;
 import com.datastax.oss.driver.api.core.metadata.schema.TableMetadata;
 import com.datastax.oss.driver.internal.core.config.typesafe.DefaultDriverConfigLoader;
-import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
 import au.com.bytecode.opencsv.CSVReader;
